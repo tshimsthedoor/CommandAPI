b636f8c34214fd84da67c27ec9b56e0cc08b4b1b
tresor <tshims79@gmail.com>
2023-05-22 15:58:44 +0200

    Initial Commit on my app

---
+++ CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : Controller
+    {
+        [HttpGet]
+        public ActionResult <IEnumerable<string>>Get()
+        {
+            return new string[] { "This", "is", "hard", "coded" };
+        }
+    }
+}
